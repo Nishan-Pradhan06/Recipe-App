2e8b83235fd7760068ef7830682666c73c2463ec
nishanpradhan <nishanpradhan500@gmail.com>
2024-09-06 10:40:14 +0545

    create-Recipe-card

---
+++ lib/views/components/card_action_button_component.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:recipe/models/card_models.dart';
+
+class CardACtionButtonComponent extends StatelessWidget {
+  final Recipe recipe;
+  const CardACtionButtonComponent({super.key, required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Icon(Icons.access_time, size: 16),
+        const SizedBox(width: 4),
+        Text('${recipe.cookingTime} min'),
+        const SizedBox(
+          width: 20,
+        ),
+        Container(
+          height: 24,
+          width: 60,
+          decoration: const BoxDecoration(
+            color: Color(0xffe0e0e0),
+            borderRadius: BorderRadius.all(
+              Radius.circular(4),
+            ),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              print('clicked');
+            },
+            child: Row(
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/saved.svg',
+                  height: 18,
+                ),
+                const Text("Save"),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/views/components/recipe_card.dart
+++ lib/views/components/recipe_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/views/components/card_text.dart';
+import 'package:recipe/views/components/card_action_button_component.dart';
 import '../../models/card_models.dart';
 
 class RecipeCard extends StatelessWidget {
@@ -11,84 +13,30 @@ class RecipeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 160,
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Card(
-        
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        elevation: 4,
-        margin: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius:
-                  const BorderRadius.vertical(top: Radius.circular(12)),
-              child: SizedBox(
-                height: 115,
-                width: double.infinity,
-                child: Image.network(
-                  recipe.imageUrl,
-                  fit: BoxFit.cover,
-                ),
-              ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 140,
+          width: 140,
+          margin: const EdgeInsets.only(right: 16),
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(20),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    recipe.title,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.access_time, size: 16),
-                          const SizedBox(width: 4),
-                          Text('${recipe.cookingTime} min'),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              debugPrint('clicked');
-                            },
-                            color: recipe.isSaved ? Colors.orange : Colors.grey,
-                            icon: Icon(
-                              recipe.isSaved
-                                  ? Icons.bookmark
-                                  : Icons.bookmark_border,
-                            ),
-                          ),
-                          Text(
-                            recipe.isSaved ? 'Saved' : '',
-                            style: TextStyle(
-                              color:
-                                  recipe.isSaved ? Colors.orange : Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+            image: DecorationImage(
+              image: NetworkImage(recipe.imageUrl),
+              fit: BoxFit.cover,
             ),
-          ],
+          ),
         ),
-      ),
+        const SizedBox(
+          height: 10,
+        ),
+        CardText(title: recipe.title),
+        CardACtionButtonComponent(recipe: recipe)
+      ],
     );
   }
 }
